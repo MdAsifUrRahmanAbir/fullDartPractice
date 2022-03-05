8f7092e1f85cd78cfb6c44e03cf1c61b263a00da
MdAsifUrRahmanAbir <mdasifurrahmanabir@gmail.com>
2022-03-05 18:41:57 +0600

    Lecture5 watching and practice done

--- bin/lecture5/lecture_05.dart
+++ bin/lecture5/lecture_05.dart
@@ -1,11 +1,10 @@
 import 'my_class.dart';
+import 'my_class2.dart';
 import 'practice_class.dart';
 
 void main(){
-  MyClass myclass1 =MyClass();
-  myclass1.display();   /// (.) is access operator    (..) double dot/ cascade operator  (...) triple dot/ spread operator
-
-
+  // MyClass myclass1 =MyClass();
+  // myclass1.display();   /// (.) is access operator    (..) double dot/ cascade operator  (...) triple dot/ spread operator
   // PracticeClass pc = PracticeClass();
   // var husName= 'Abir';
   // pc.setHusName(husName);
@@ -13,7 +12,21 @@ void main(){
   // print(pc.wifeName);
   // pc.statusDisplay(pc.getName(), pc.wifeName);
 
-  myclass1.sum(a1: 10, a2: 10);
-  myclass1.myListMaker('name', 'name2', 'name3').forEach((element) { print(element);});
+  // myclass1.sum(a1: 10, a2: 10);
+  // myclass1.myListMaker('name', 'name2', 'name3').forEach((element) { print(element);});
 
+  var m2 = MyClass2();
+
+  //print('User info:: Username: ${m2.userName}\nPhone: ${m2.userNumber}\nEmail: ${m2.userMail}\nCGPA: ${m2.cgpa}\nActive: ${m2.isActive}');
+
+
+  m2.userName= 'Abir';
+  m2.userNumber = 01877348044;
+  m2.userMail = 'mdasifurrahmanabir@gmail.com';
+  m2.cgpa = 3.5;
+  m2.isActive = true;
+  m2.subject = ['Bangla', 'Math', 'English'];
+
+  print('User info:: Username: ${m2.userName}\nPhone: ${m2.userNumber}\nEmail: ${m2.userMail}\nCGPA: ${m2.cgpa}\nActive: ${m2.isActive}');
+  for (var element in m2.subject) {print(element);}
 }
